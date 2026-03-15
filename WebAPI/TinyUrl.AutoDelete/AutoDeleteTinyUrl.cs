999a6e88e98da18bf64fcee8234df7e5c886f550
Karthick D <dkarthick2810@outlook.com>
2026-03-15 16:40:12 +0530

    Added TInyUrl  Delete Cron - Azure Function

---
+++ WebAPI/TinyUrl.AutoDelete/AutoDeleteTinyUrl.cs
@@ -0,0 +1,42 @@
+using Azure.Core;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+
+namespace TinyUrl.AutoDelete
+{
+    public class AutoDeleteTinyUrl
+    {
+        private readonly ILogger _logger;
+
+        public AutoDeleteTinyUrl(ILoggerFactory loggerFactory)
+        { 
+            _logger = loggerFactory.CreateLogger<AutoDeleteTinyUrl>();
+        }
+
+        [Function("AutoDeleteTinyUrl")]
+        public async Task Run([TimerTrigger("*/60 * * * * *")] TimerInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function started at: {DateTime.Now}");
+
+            string? apiUrl = Environment.GetEnvironmentVariable("TargetApiUrl");
+            string? sectretTOken = Environment.GetEnvironmentVariable("SecretToken");
+
+            HttpClient httpClient = new HttpClient();
+            var encodedSecret = Uri.EscapeDataString(sectretTOken);
+
+            apiUrl = apiUrl + "?secretCode=" + encodedSecret;
+            var response = await httpClient.DeleteAsync(apiUrl);
+            if(response.IsSuccessStatusCode)
+            _logger.LogInformation($"Timer Trigger Response : {response.Content.ToString()}");
+
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+    }
+}
